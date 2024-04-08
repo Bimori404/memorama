0d410443ca024f437c9ea8567517dd5238e474bd
/Bimori <106877704+Bimori404@users.noreply.github.com>
2024-04-07 22:29:29 -0600

    dot

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // array const de usuarios
+console.log("leemos el array actual");
 const actual = [
     {
         nombre: 'Abel',
@@ -77,6 +78,7 @@ const actual = [
         imagen: '/media/actual/Cynthia_actual.JPG'
     },
 ];
+console.log("leemos el array infancia");
 const infancia = [
     {
         nombre: 'Abel',
@@ -156,11 +158,19 @@ const infancia = [
     },
 ];
 
-// duplica las cartas
+/* 
+    La sintaxis [...array] se utiliza para copiar los elementos de
+    un array en un nuevo array. En este caso, se están combinando
+    los elementos de dos arrays existentes en uno nuevo.
+
+    basicamente duplica las cartas
+*/
+console.log("copiamos elementos de un array en un nuevo array");
 const cartas = [...actual, ...infancia];
 
 // shuffle del array de cartas
 function shuffle(array) {
+    console.log("function shufle");
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -170,8 +180,10 @@ function shuffle(array) {
 
 // crea las cartas
 function crearCartas() {
+    console.log("function crearCartas");
     const container = document.querySelector('.container');
     shuffle(cartas).forEach((carta, index) => {
+        console.log("se llama a function shufle");
         const card = document.createElement('div');
         card.classList.add('card');
         card.dataset.id = index;
@@ -194,28 +206,43 @@ function crearCartas() {
     });
 }
 
+console.log("se llama a function crearCartas");
 crearCartas();
 
 // logica
 // let puntos = 30;
-let puntos = 30;
+console.log("asignamos puntos iniciales");
+let puntos = 0;
+console.log("asignamos array 'seleccionadas' para guardar las posiciones y comparar");
 let seleccionadas = [];
 
 document.querySelectorAll('.card').forEach(card => {
+    console.log("seleccionamos las .card's creadas");
     card.addEventListener('click', () => {
+        console.log("card : reconocemos eventos del raton");
         if (seleccionadas.length < 2 && !card.classList.contains('is-flipped')) {
+            console.log("if : select -> add class .is-flipped and add to 'card'");
             card.classList.add('is-flipped');
             seleccionadas.push(card);
             if (seleccionadas.length === 2) {
+                console.log("if : verifica si hay dos elementos");
                 const carta1 = seleccionadas[0].dataset.id;
+                console.log(carta1);
                 const carta2 = seleccionadas[1].dataset.id;
+                console.log(carta2);
                 if (cartas[carta1].nombre === cartas[carta2].nombre) {
+                    console.log("if : select is equals actual/infancia");
                     puntos++;
+                    console.log("ponts ++");
                     document.getElementById('lpun').textContent = `Puntos: ${puntos}`;
+                    console.log("limpiamos el arreglo 'seleccionadas'");
                     seleccionadas = [];
                 } else {
+                    console.log("else : if is not equals");
                     setTimeout(() => {
+                        console.log("setTimeout : delete class 'is-flipped' to card's");
                         seleccionadas.forEach(card => card.classList.remove('is-flipped'));
+                        console.log("limpiamos el arreglo 'seleccionadas'");
                         seleccionadas = [];
                         // puntos--;
                         // document.getElementById('lpun').textContent = `Puntos: ${puntos}`;
